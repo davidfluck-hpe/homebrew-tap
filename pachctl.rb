39a6c5bde6171cdce02dd086d952f963a89d429e
David Fluck <david.fluck@hpe.com>
2024-04-03 11:24:10 -0400

    Use on_* blocks to expand platform/arch selection logic.

--- pachctl.rb
+++ pachctl.rb
@@ -6,31 +6,33 @@ class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
   version "v2.9.3"
 
-  bottle do
-    root_url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3"
-    sha256 arm64_ventura: "a4aa3db93bc05741a4879ae969116a9778c55434270523b717e2bfbdace63d6b"
+  on_macos do
+    on_arm do
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3/pachctl_2.9.3_darwin_arm64.zip"
+      sha256 "f76c1ad95d8295ccddc38ae90f73a7b5d3c36dd728e433221e425926b3d4a56f"
+    end
+
+    on_intel do
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3/pachctl_2.9.3_darwin_amd64.zip"
+      sha256 "b3f4ec26577529220ce7420dba7cf4b117aa29945f58dcf83f6163358e1ca8c6"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3/pachctl_2.9.3_linux_arm64.zip"
+      sha256 "049cb11806f5870338ea9ca60bb49d2af3641d3a5da23e84cda5e12ebbeea545"
+    end
+
+    on_intel do
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3/pachctl_2.9.3_linux_amd64.zip"
+      sha256 "6ce767700da2b334ce239377203c49d10486b2c435dd5ebfeb70e51a1368aeb7"
+    end
   end
 
   def install
     bin.install buildpath/"pachctl"
   end
-
-  # if Hardware::CPU.intel?
-  #   url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3/pachctl_2.9.3_darwin_amd64.zip"
-  #   sha256 "c50837e45228cff53a09e95d37ff607b0132efd5bc252bbe359aa949df771114"
-
-  #   def install
-  #     bin.install buildpath/"pachctl"
-  #   end
-  # end
-  # if Hardware::CPU.arm?
-  #   url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.3/pachctl_2.9.3_darwin_arm64.zip"
-  #   sha256 "6d1834cc627d503c88532cabd9b05ca4b6a48065c83054ad4ad8d3c4289901f4"
-
-  #   def install
-  #     bin.install buildpath/"pachctl"
-  #   end
-  # end
 
   test do
     system "#{bin}/pachctl version"
